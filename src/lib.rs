c53c36702ebfdc86ba6a808da7ef3a5c3dd8a1aa
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-09-28 08:40:00 +0200

    Added panic-over-itm / abort-on-panic as features

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+#![cfg_attr(feature = "panic-over-itm", feature(core_intrinsics))]
+#![cfg_attr(feature = "panic-over-itm", feature(lang_items))]
+
 #![no_std]
 
+
 extern crate s32k144;
+#[macro_use]
 extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate bit_field;
@@ -8,3 +13,6 @@ extern crate bit_field;
 pub mod led;
 pub mod wdog;
 pub mod can;
+
+#[cfg(feature = "panic-over-itm")]
+mod panic;

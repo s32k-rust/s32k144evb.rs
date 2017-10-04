909ccdbaa0115899c32b4a388ef1b31a81ac8080
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-04 21:38:31 +0200

    Added more general algorithm for resolving baud

--- src/lpuart.rs
+++ src/lpuart.rs
@@ -1,5 +1,9 @@
 use s32k144::LPUART1;
 
+pub enum UartError {
+    UnsatisfiableBaud,
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct UartSettings {
     pub baudrate: u32,
@@ -40,7 +44,7 @@ pub enum Parity {
     O,
 }
 
-pub fn configure(lpuart: &LPUART1, settings: UartSettings, source_frequency: u32) {
+pub fn configure(lpuart: &LPUART1, settings: UartSettings, source_frequency: u32) -> Result<(), UartError> {
     // disable receiver and transmiter
     lpuart.ctrl.modify(|_r, w| w
                        .te().clear_bit()
@@ -48,11 +52,14 @@ pub fn configure(lpuart: &LPUART1, settings: UartSettings, source_frequency: u32
     );
 
     // TODO: check that divisor is a sensible value
-    let divisor = source_frequency / (settings.baudrate * 16);
+    let (oversampling_ratio, divisor) = find_decent_div(source_frequency, settings.baudrate)?;
+    let bothedge = oversampling_ratio < 8;
     
     lpuart.baud.write(|w| unsafe{ w
                                   .m10().bit(settings.data_bits == DataBits::B10)
                                   .sbns().bit(settings.stop_bits == StopBits::B2)
+                                  .bothedge().bit(bothedge)
+                                  .osr().bits(oversampling_ratio)
                                   .sbr().bits(divisor as u16)
     });
 
@@ -69,9 +76,41 @@ pub fn configure(lpuart: &LPUART1, settings: UartSettings, source_frequency: u32
                        //.re().set_bit()
     );
 
+    Ok(())
 }
 
 pub fn transmit(lpuart: &LPUART1, data: u8) {
     lpuart.data.write(|w| unsafe{w.bits(data as u32)});
+}
+
+fn find_decent_div(source: u32, baud: u32) -> Result<(u8, u16), UartError> {
+    const OVERSAMPLING_MIN: u32 = 4;
+    const OVERSAMPLING_MAX: u32 = 32;
+
+    const DIV_MIN: u32 = 1;
+    const DIV_MAX: u32 = 8191;
+    
+    let ratio = (source + baud/2)/baud;
+    let alternative_ratio = {
+        if ratio == source/baud {
+            ratio + 1
+        } else {
+            ratio - 1
+        }
+    };
+
+    for i in (OVERSAMPLING_MIN..OVERSAMPLING_MAX+1).rev() {
+        if ratio%i == 0 && ratio/i >= DIV_MIN && ratio/i <= DIV_MAX {
+            return Ok((i as u8, (ratio/i) as u16));
+        }
+    }
+
+    for i in (OVERSAMPLING_MIN..OVERSAMPLING_MAX+1).rev() {
+        if alternative_ratio%i == 0 && alternative_ratio/i >= DIV_MIN && alternative_ratio/i <= DIV_MAX {
+            return Ok((i as u8, (alternative_ratio/i) as u16));
+        }
+    }
+
+    Err(UartError::UnsatisfiableBaud)
 }
 
